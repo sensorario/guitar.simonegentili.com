3044361b3b202f4a5a6c6b3c0181490d53183b4c
Simone Gentili <sensorario@gmail.com>
2025-11-24 21:27:59 +0100

    feat(guitar): initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>guitar.simonegentili.com</h1>
+      <p>Welcome to the guitar simulator!</p>
+    </>
+  )
+}
+
+export default App
